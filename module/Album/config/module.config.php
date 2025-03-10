80d11e03a7085734002302cf83ce5a3c3e8664e1
Lucas Silva Piedade <lucas.piedade700@gmail.com>
2025-03-09 22:03:54 -0300

    Tutorial Laminas

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Album;
+
+use Laminas\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
+use Album\Model\AlbumTableFactory;
+use Laminas\Router\Http\Segment;
+
+return [
+    'controllers' => [
+        'factories' => [
+            Controller\AlbumController::class => ReflectionBasedAbstractFactory::class
+        
+        ],
+    ],
+
+    // The following section is new and should be added to your file:
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+
+    'service_manager' => [
+        'factories' => [
+            Model\AlbumTable::class => AlbumTableFactory::class,
+        ],
+
+    ],
+];
